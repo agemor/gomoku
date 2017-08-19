8ac220fb30b42985e7345ffb84dceb7c74f70f91
HyunJun Kim <hyunjun.leo.kim@gmail.com>
2017-08-20 01:55:17 +0900

    Update lobby

--- src/OmokLobby.js
+++ src/OmokLobby.js
@@ -5,6 +5,8 @@ export default class OmokLobby {
     constructor() {
         this.serverConnectionHandlers = [];
         this.serverDisconnectionHandlers = [];
+        this.serverErrorHandlers = [];
+        this.opponentFoundHandlers = [];
     }
 
     connectServer(host) {
@@ -19,11 +21,28 @@ export default class OmokLobby {
                 this.serverDisconnectionHandlers[i]();
             }
         })
+        this.serverConnection.on('connect_error', (error) => {
+            for (let i in this.serverErrorHandlers) {
+                this.serverErrorHandlers[i](error);
+            }
+        })
         this.serverConnection.on('status', function (data) {
             console.log(data);
             socket.emit('my other event', { my: 'data' });
         });
+    }
 
+    findOpponent(callback) {
+        this.serverConnection.emit('find opponent');
+        this.serverConnection.on('room created',  (roomToken, roomId)=>{
+            for (let i in this.opponentFoundHandlers) {
+                this.opponentFoundHandlers[i](roomToken, roomId);
+            }
+        });
+    }
+
+    onServerClosed(handler) {
+        this.serverErrorHandlers.push(handler);
     }
 
     onServerConnected(handler) {
@@ -34,9 +53,7 @@ export default class OmokLobby {
         this.serverDisconnectionHandlers.push(handler);
     }
 
-    joinGame() {
-
+    onOpponentFound(handler) {
+        this.opponentFoundHandlers.push(handler);
     }
-
-    onRequest()
 }
--- src/lobby.js
+++ src/lobby.js
@@ -1,5 +1,24 @@
 import OmokLobby from "./OmokLobby";
 
+// 상태 표시 텍스트
+let statusText = document.getElementById("statusText");
+
 let lobby = new OmokLobby();
 
 lobby.connectServer("http://localhost:5555");
+
+statusText.textContent = "서버 접속 중...";
+
+lobby.onServerClosed((error)=>{
+    statusText.textContent = "서버가 닫혀 있습니다.";
+});
+
+lobby.onServerConnected(()=>{
+    lobby.findOpponent();
+    statusText.textContent = "대전할 상대방 기다리는 중...";
+});
+
+lobby.onOpponentFound((roomToken, roomId)=>{
+    statusText.textContent = "상대방을 찾았습니다!" + roomToken + "///////" + roomId;
+    window.location.assign("./game.html?id=" + roomId + "&token=" + roomToken);
+})
