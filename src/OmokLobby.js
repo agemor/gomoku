b96c632568704da1b9d17ee52c92e25acbe3ce70
HyunJun Kim <hyunjun.leo.kim@gmail.com>
2017-08-23 05:06:17 +0900

    Update lobby to meet the new protocol

--- src/OmokLobby.js
+++ src/OmokLobby.js
@@ -3,48 +3,166 @@ import io from "socket.io-client";
 export default class OmokLobby {
 
     constructor() {
+
+        // 서버 상태 정보
+        this.connected = false;
+        this.recentErrorMessage = "";
+
+        // 유저 인증 정보
+        this.loggined = false;
+        this.playerId = null;
+        this.playerKey = null;
+
+        // 방 입장 정보
+        this.matchFound = false;
+        this.roomId = null;
+        this.roomKey = null;
+
+        // 이벤트 핸들러
         this.serverConnectionHandlers = [];
         this.serverDisconnectionHandlers = [];
         this.serverErrorHandlers = [];
+        this.serverMessageHandlers = [];
     }
 
+    /**
+     * 로비 서버에 접속한다.
+     * 
+     * @param {string} host 
+     */
     connectServer(host) {
-        this.serverConnection = io.connect(host);
-        this.serverConnection.on('connect', () => {
+
+        this.socket = io.connect(host);
+
+        this.socket.on("connect", () => {
+
+            this.connected = true;
+
             for (let i in this.serverConnectionHandlers) {
-                this.serverConnectionHandlers[i](this.serverConnection.connected);
+                this.serverConnectionHandlers[i](this.socket.connected);
             }
         });
-        this.serverConnection.on('disconnect', () => {
+
+        this.socket.on("disconnect", () => {
+
+            this.connected = false;
+
             for (let i in this.serverDisconnectionHandlers) {
                 this.serverDisconnectionHandlers[i]();
             }
-        })
-        this.serverConnection.on('connect_error', (error) => {
+        });
+
+        this.socket.on("connect_error", (error) => {
+
+            this.connected = false;
+
             for (let i in this.serverErrorHandlers) {
                 this.serverErrorHandlers[i](error);
             }
-        })
-        this.serverConnection.on('status', function (data) {
-            console.log(data);
-            socket.emit('my other event', { my: 'data' });
+        });
+
+        this.socket.on("server message", (data) => {
+
+            for (let i in this.serverMessageHandlers) {
+                this.serverMessageHandlers[i](data.message);
+            }
         });
     }
 
-    findOpponent(callback) {
-        this.serverConnection.emit('find opponent');
-        this.serverConnection.on('room created',  (roomId, roomToken)=>{
-            callback(roomId, roomToken);
+    /**
+     * 로그인을 시도한다. 성공할 경우 인증 데이터를 받을 수 있다.
+     * 
+     * @param {string} nickname 
+     * @param {function} callback 
+     */
+    login(nickname, callback) {
+
+        if (!this.connected) return;
+
+        this.socket.emit("login", nickname);
+
+        this.socket.on("login success", (playerId, playerKey) => {
+
+            this.loggined = true;
+            this.playerId = playerId;
+            this.playerKey = playerKey;
+
+            callback(true);
         });
+
+        this.socket.on("cannot login", (error) => {
+
+            this.loggined = false;
+            this.recentErrorMessage = error.message;
+
+            callback(false);
+        });
+    }
+
+    /**
+     * 플레이 가능한 상대방을 찾는다.
+     * 
+     * @param {string} callback 
+     */
+    findMatch(callback) {
+
+        if (!this.loggined) return;
+
+        this.socket.emit("find match", this.playerId, this.playerKey);
+
+        this.socket.on("match found",  (roomId, roomKey) => {
+
+            this.matchFound = true;
+            this.roomId = roomId;
+            this.roomKey = roomKey;
+
+            callback(true);
+        });
+
+        this.socket.on("cannot find match", (error) => {
+
+            this.matchFound = false;
+            this.recentErrorMessage = error.message;
+
+            callback(false);
+        });
+    }
+
+    /**
+     * 게임 방으로 이동한다.
+     */
+    redirectToRoom() {
+
+        if (this.roomId == null) return;
+
+        let url = "./game.html?roomId=" + this.roomId;
+        url += this.roomKey != null ? "&key=" + this.roomKey : "";
+        url += this.loggined ? "&playerId=" + this.playerId + "&playerKey=" + this.playerKey : "";
+
+        window.location.assign(url);
     }
 
     getRandomRoom(callback) {
-        this.serverConnection.emit('get random room id');
-        this.serverConnection.on('random room id',  (roomId)=>{
-            callback(roomId);
-        });
-        this.serverConnection.on('no random room',  ()=>{
-            callback(null);
+
+        if (!this.connected) return;
+
+        this.socket.emit('get random room');
+
+        this.socket.on('random room',  (roomId)=>{
+
+            if (roomId != "") {
+
+                this.roomId = roomId;
+
+                callback(true);
+            }
+
+            else {
+
+                this.roomId = null;
+
+                callback(false);
+            }
         });
     }
 
@@ -58,5 +176,13 @@ export default class OmokLobby {
 
     onServerDisconnected(handler) {
         this.serverDisconnectionHandlers.push(handler);
+    }
+
+    onServerMessage(handler) {
+        this.serverMessageHandlers.push(handler);
+    }
+
+    getRecentErrorMessage() {
+        return this.recentErrorMessage;
     }
 }
--- src/lobby.js
+++ src/lobby.js
@@ -1,10 +1,15 @@
+import KoreanNameGenerator from "korean-name-generator";
 import OmokLobby from "./OmokLobby";
+
+let host = "http://192.168.0.19:5555";
 
 // DOM 객체 레퍼런스
 let statusText = document.getElementById("statusText");
 let nicknameInputText = document.getElementById("nicknameInputText");
 let startButton = document.getElementById("startButton");
 let observeButton = document.getElementById("observeButton");
+
+let setStatus = (text) => statusText.textContent = text;
 
 // 이벤트 리스너 할당
 startButton.addEventListener("click", onStartClicked);
@@ -13,57 +18,106 @@ observeButton.addEventListener("click", onObserveClicked);
 // 로비 객체 생성
 let lobby = new OmokLobby();
 
-// 서버 접속 시도
-let connected = false;
-let searching = false;
-lobby.connectServer("http://192.168.0.19:5555");
-statusText.textContent = "서버 접속 중...";
+lobby.connectServer(host);
+setStatus("서버 접속 중...");
 
 lobby.onServerClosed((error)=>{
-    statusText.textContent = "아쉽게도 지금은 서버가 닫혀 있습니다.";
-    startButton.disabled = true;
-    observeButton.disabled = true;
+    setStatus("아쉽게도 지금은 서버가 닫혀 있습니다.");
 });
 
 lobby.onServerConnected(()=>{
     connected = true;
-    statusText.textContent = "지금 바로 게임을 시작해 보세요!";
+    setStatus("지금 바로 게임을 시작해 보세요!");
 });
 
+// 대기 상태
+let waiting = false;
 
-function onStartClicked() {
-    if (connected && !searching) {
+let toggleWaiting = (waitingOn) => {
 
-        // 상대방 탐색
-        lobby.findOpponent((roomId, roomToken)=>{
+    waiting = waitingOn;
 
-            // 게임 시작
-            window.location.assign("./game.html?id=" + roomId + "&token=" + roomToken);
-        });
-
-        statusText.textContent = "대전할 상대방 찾는 중...";
+    if (waitingOn) {
 
         nicknameInputText.disabled = true;
         startButton.disabled = true;
         observeButton.setAttribute('disabled', true);
-        searching = true;
+        
+    } else {
+
+        nicknameInputText.disabled = false;
+        startButton.disabled = false;
+        observeButton.removeAttribute("disabled");
     }
 }
 
-function onObserveClicked() {
-    if (connected && !searching) {
-        // 랜덤한 방 id 요청
-        lobby.getRandomRoom((roomId)=>{
-            if (roomId == null) {
-                statusText.textContent = "현재 게임 중인 방이 없습니다.";
-                observeButton.removeAttribute("disabled");
-                startButton.disabled = false;
-            } else {
-                window.location.assign("./game.html?id=" + roomId);
+// 게임 시작 버튼이 눌렸을 때
+function onStartClicked() {
+
+    if (lobby.connected && !waiting) {
+
+        toggleWaiting(true);
+
+        // 닉네임이 입력되지 않았다면 자동 생성
+        if (nicknameInputText.value.length < 1) {
+            nicknameInputText.value = KoreanNameGenerator.generate(Math.random() > 0.5);
+        }
+
+        // 닉네임 정제
+        let nickname = nicknameInputText.value.substring(0, 10);
+
+        // 로그인
+        lobby.login(nickname, (loginSuccess) => {
+
+            if (!loginSuccess) {
+                
+                toggleWaiting(false);
+                setStatus(lobby.getRecentErrorMessage());
+
+                return;
             }
+
+            // 대전할 상대방 탐색
+            lobby.findMatch((findSuccess) => {
+
+                if (!findSuccess) {
+                    
+                    toggleWaiting(false);
+                    setStatus(lobby.getRecentErrorMessage());
+
+                    return;
+                }
+
+                // 상대방 발견
+                lobby.redirectToRoom();
+            });
         });
-        statusText.textContent = "게임 중인 방 탐색 중...";
-        observeButton.setAttribute('disabled', true);
-        startButton.disabled = true;
+
+        setStatus("대전할 상대방 찾는 중...");
+    }
+}
+
+// 관전 버튼이 눌렸을 때
+function onObserveClicked() {
+
+    if (lobby.connected && !waiting) {
+
+        toggleWaiting(true);
+
+        // 랜덤한 방 선택
+        lobby.getRandomRoom((getSuccess) => {
+
+            if (!getSuccess) {
+                
+                toggleWaiting(false);
+                setStatus("현재 관전 가능한 방이 없습니다.");
+
+                return;
+            }
+
+            lobby.redirectToRoom();
+        });
+
+        setStatus("대전 중인 방 찾는 중...");
     }
 }
